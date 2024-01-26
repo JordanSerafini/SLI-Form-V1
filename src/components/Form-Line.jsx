ec29f846eff5e16d2c0eb59c01cb532b55282758
Jordan - Solution Logique <j.serafini@solution-logique.local>
2024-01-26 10:16:06 +0100

    first comit :sparkles:

--- src/components/Form-Line.jsx
+++ src/components/Form-Line.jsx
@@ -8,13 +8,13 @@ function FormLine({ title, children }) {
   const childrenArray = React.Children.toArray(children);
 
   return (
-    <div className="bg-cream border-brown border-4 p-4 shadow-custom w-9/10 mt-10 font-lato flex flex-col  gap-8">
-        <h3 className="text-xl font-bold border-b-4 pb-4 border-brown p-2">
+    <div className="bg-white rounded-xl border-brown border-4 p-4 shadow-custom w-9/10 mt-10 font-lato flex flex-col gap-6">
+        <h3 className="text-lg md:text-xl lg:text-2xl font-bold border-b-4 pb-4 border-brown p-2">
           {title}
         </h3>
 
         {childrenArray.map((child, index) => (
-          <section key={index} className="text-base flex flex-col items-center justify-center">
+          <section key={index} className="text-base flex flex-col items-center justify-center gap-2">
             <p className="">
               {child}
             </p>
