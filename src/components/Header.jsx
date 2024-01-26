544facd65e4fff94cbf0e5056febae574b5a9678
Jordan - Solution Logique <j.serafini@solution-logique.local>
2024-01-26 15:51:18 +0100

    first comit :sparkles:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import logoSLI from '../assets/logoSLI.png'
 
 function Header() {
+  
   return (
-    <div className='gap-3 rounded-b-full text-white h-20 bg-gradient-to-r from-blue-light to-blue-strong flex items-center justify-center text-base font-semibold' style={{ position: 'sticky', top: '0', zIndex: '100' }}>
+    <div className=' shadow-header gap-3 rounded-b-full text-white h-20 bg-gradient-to-r from-blue-light to-blue-strong flex items-center justify-center text-base font-semibold' style={{ position: 'sticky', top: '0', zIndex: '100' }}>
         
         <img src={logoSLI} alt="" className=' w-2/10 rounded-full'/>
         
