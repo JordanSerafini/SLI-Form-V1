76b6e9d25985aa0d3750ab30440d970b17ab7d28
Jordan - Solution Logique <j.serafini@solution-logique.local>
2024-01-26 15:36:46 +0100

    first comit :sparkles:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className=' gap-3 text-white h-20 bg-blue-strong flex items-center justify-center text-xl' style={{ position: 'sticky', top: '0', zIndex: '100' }}>
         
-        <img src={logoSLI} alt="" className=' w-1/10 rounded-full'/>
+        <img src={logoSLI} alt="" className=' w-2/10 rounded-full'/>
         
           - Questionnaire satisfaction</div>
   )
