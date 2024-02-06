2868e0b501df2488f84324db9891249174c3fb54
JordanSerafini <jordan@solution-logique.fr>
2024-02-06 16:20:51 +0100

    :construction: work on front

--- src/components/Middleware/PrivateRoute.jsx
+++ src/components/Middleware/PrivateRoute.jsx
@@ -3,12 +3,11 @@ import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function PrivateRoute({ element }) {
-  // Récupère le JWT depuis le localStorage avec la clé 'token'
-  //const jwt = localStorage.getItem('token');
+  const jwt = localStorage.getItem('token');
 
-  const isAuthenticated = true;
+  //const isAuthenticated = true;
 
-  if (isAuthenticated) {
+  if (jwt) {
       return element;
   } else {
      
