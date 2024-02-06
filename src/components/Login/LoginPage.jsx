3429f7904679185d0ce0cf6b823dcb4aab297f29
JordanSerafini <jordan@solution-logique.fr>
2024-02-06 15:03:16 +0100

    :construction: work on front

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -9,7 +9,7 @@ function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const navigate = useNavigate(); // Hook pour la redirection
+    const navigate = useNavigate(); 
   
     const handleLogin = async (event) => {
       event.preventDefault();
