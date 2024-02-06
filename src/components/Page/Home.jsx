e8d5e3e282b7056754dd87afe5c7f34200ae298d
JordanSerafini <jordan@solution-logique.fr>
2024-02-06 15:41:25 +0100

    :construction: work on front

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -7,24 +7,25 @@ function Home() {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    // Utilisez useEffect pour effectuer la requête lorsque le composant est monté
     const fetchUserData = async () => {
         const email = localStorage.getItem('email');
       try {
         const response = await axios.post(`${apiUrl}/getUtilisateurInfo`, {
           email: email
         });
+        console.log(response);
+        console.log(response.data);
+
 
         if (response.data) {
-          // Si vous recevez des données valides, mettez-les dans l'état
           setUserData(response.data);
+          
         }
       } catch (error) {
         console.error('Erreur lors de la récupération des données de l\'utilisateur:', error);
       }
     };
 
-    // Appelez la fonction pour récupérer les données de l'utilisateur
     fetchUserData();
   }, []); 
 
@@ -36,7 +37,6 @@ function Home() {
           <h2>Informations de l&apos;utilisateur :</h2>
           <p>Nom : {userData.name}</p>
           <p>Email : {userData.email}</p>
-          {/* Ajoutez d'autres informations de l'utilisateur ici si nécessaire */}
         </div>
       )}
     </div>
