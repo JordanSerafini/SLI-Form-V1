a4f411598e7b78e6114d82421b55ff6c90dd9f13
JordanSerafini <jordan@solution-logique.fr>
2024-02-07 13:10:35 +0100

    :construction: adding toastify

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -3,114 +3,77 @@ import { useState, useEffect, useContext } from "react";
 import RatingContext from "../../context/RatingContext";
 import backUrl from "../../Axios/backUrl";
 
-
 import Header from "../Header/Header";
 import LogoutBtn from "../Login/LogoutBtn";
 
-const apiUrl = backUrl; 
-
 function Home() {
-
   const { showToast } = useContext(RatingContext);
-
   const [userData, setUserData] = useState(null);
   const [showEmailInput, setShowEmailInput] = useState(false);
   const [emailToSend, setEmailToSend] = useState("");
   const token = localStorage.getItem("token");
 
-  // Fetch user data en base de donnée
   useEffect(() => {
-    const fetchUserData = async () => {
-      if (!token) {
-        console.error("Token non trouvé dans le localStorage");
-        return;
-      }
+    if (!token) {
+      console.error("Token non trouvé dans le localStorage");
+      return;
+    }
 
+    const fetchUserData = async () => {
       try {
-        const response = await axios.get(`${apiUrl}/getUtilisateurInfo`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+        const response = await axios.get(`${backUrl}/getUtilisateurInfo`, {
+          headers: { Authorization: `Bearer ${token}` },
         });
 
-        if (response.data) {
-          setUserData(response.data);
-        }
+        setUserData(response.data);
       } catch (error) {
-        console.error(
-          "Erreur lors de la récupération des données de l'utilisateur:",
-          error
-        );
+        console.error("Erreur lors de la récupération des données de l'utilisateur:", error);
       }
     };
 
     fetchUserData();
+  }, [token]);
 
-  }, [token ]);
-
-  // Gestion Toast de bienvenue
   useEffect(() => {
     if (userData) {
-      showToast(`Bienvenue ${userData.name}`, {
-        position: "top-center",
-        autoClose: 3000, 
-      });    
+      showToast(`Bienvenue ${userData.name}`, { position: "top-center", autoClose: 3000 });
     }
   }, [userData, showToast]);
 
-  // Envoi du formulaire de satisfaction
   const sendForm = async () => {
     if (!emailToSend) {
-      showToast("Veuillez entrer une adresse e-mail.", {
-        position: "top-center",
-        autoClose: 3000,
-      });
+      showToast("Veuillez entrer une adresse e-mail.", { position: "top-center", autoClose: 3000 });
       return;
     }
     if (!token) {
-      showToast("Vous devez vous connecter pour envoyer le formulaire.", {
-        position: "top-center",
-        autoClose: 3000,
-      });
+      showToast("Vous devez vous connecter pour envoyer le formulaire.", { position: "top-center", autoClose: 3000 });
       return;
     }
-  
+
     try {
       const response = await axios.post(
-        `${apiUrl}/sendForm`,
-        //`https://sli-form.netlify.app/sendForm`,
-        { email: emailToSend }, 
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+        `${backUrl}/sendForm`,
+        { email: emailToSend },
+        { headers: { Authorization: `Bearer ${token}` } }
       );
-  
-      if (response.data) {
-        showToast(response.data.message, {
-          position: "top-center",
-          autoClose: 3000,
-        });
-      }
+
+      showToast(response.data.message, { position: "top-center", autoClose: 3000 });
     } catch (error) {
       console.error("Erreur lors de l'envoi du formulaire de satisfaction:", error);
     }
   };
 
   return (
-    <div className="bg-cream h-screen flex flex-col items-center ">
+    <div className="bg-cream h-screen flex flex-col items-center">
       <Header />
       {userData && (
         <div className="bg-white rounded-xl border-brownperso border-4 p-2 shadow-custom mt-8 flex flex-col font-playfair w-9.5/10 h-8/10">
-          <div className="text-sm" onClick={() => setShowEmailInput(true)}>
+          <div className="text-sm" onClick={() => setShowEmailInput(!showEmailInput)}>
             Envoyer un formulaire de satisfaction
           </div>
-
-          {/* Affichage de l'input s'il est visible */}
           {showEmailInput && (
             <div>
-              <input 
+              <input
                 type="email"
                 placeholder="Adresse e-mail"
                 value={emailToSend}
@@ -119,7 +82,6 @@ function Home() {
               <button onClick={sendForm}>Envoyer</button>
             </div>
           )}
-          
         </div>
       )}
       <LogoutBtn />
