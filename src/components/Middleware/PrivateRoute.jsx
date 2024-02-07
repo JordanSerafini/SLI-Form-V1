2857098b91b0229dfbc1345162b578f0a75b265c
JordanSerafini <jordan@solution-logique.fr>
2024-02-07 14:50:50 +0100

    :construction: envoi mail ok, toast ok, page form protéger par token envoyé par mail ok :sparkles: :sparkles:

--- src/components/Middleware/PrivateRoute.jsx
+++ src/components/Middleware/PrivateRoute.jsx
@@ -1,4 +1,3 @@
-import { Navigate } from 'react-router-dom';
 import { useEffect, useContext } from 'react';
 import RatingContext from "../../context/RatingContext";
 import PropTypes from 'prop-types';
@@ -6,38 +5,23 @@ import PropTypes from 'prop-types';
 function PrivateRoute({ element }) {
   const { showToast } = useContext(RatingContext);
   const jwt = localStorage.getItem('token');
-
+  
+  // Vérifie si le token est présent
   useEffect(() => {
-    // Vérifier si le token est présent dans le header Authorization
-    const tokenFromHeader = getTokenFromHeader();
-
-    // Si le token est présent dans le header, afficher un message
-    if (tokenFromHeader) {
-      showToast("Vous avez été redirigé vers cette page avec un token.");
-    } else {
-      // Si aucun token n'est présent dans le header, rediriger vers la page de connexion
-      showToast("Vous devez vous connecter pour accéder à cette page.");
+    if (!jwt) {
+      showToast("Vous devez être connecté pour accéder à cette page", { position: "top-center", autoClose: 3000 });
       navigateToLogin(); // Redirection vers la page de connexion
     }
-  }, [showToast]);
+  }, [jwt, showToast]);
 
   // Fonction pour effectuer la redirection vers la page de connexion
   const navigateToLogin = () => {
-    return <Navigate to="/login" />;
+    window.location.replace('/login'); // Redirection via le changement de l'URL
   };
 
   // Rend le composant cible si le token est présent
   return element;
 }
-
-// Fonction utilitaire pour extraire le token du header Authorization
-const getTokenFromHeader = () => {
-  const authHeader = localStorage.getItem('Authorization');
-  if (authHeader && authHeader.startsWith('Bearer ')) {
-    return authHeader.substring(7);
-  }
-  return null;
-};
 
 PrivateRoute.propTypes = {
   element: PropTypes.element.isRequired,
