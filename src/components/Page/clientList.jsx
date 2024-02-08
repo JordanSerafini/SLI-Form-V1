606c2d7e9ad9c4be21b96a5181d172d0e5cd4c22
JordanSerafini <jordan@solution-logique.fr>
2024-02-08 16:41:23 +0100

    :sparkles: working on front , fetch client list, pagination, seems ok

--- src/components/Page/clientList.jsx
+++ src/components/Page/clientList.jsx
@@ -4,9 +4,11 @@ import RatingContext from "../../context/RatingContext";
 import { useContext, useEffect, useState } from "react";
 
 function ClientList() {
-  const { clientList, setClientList } = useContext(RatingContext);
   const { showToast } = useContext(RatingContext);
+  const [clientList, setClientList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const clientsPerPage = 50;
 
   useEffect(() => {
     const fetchClientList = async () => {
@@ -15,14 +17,30 @@ function ClientList() {
         setClientList(response.data);
         setLoading(false);
       } catch (error) {
-        console.error("Erreur lors de la récupération de la liste des clients :", error);
-        showToast("Erreur lors de la récupération de la liste des clients", { type: "error" });
+        console.error(
+          "Erreur lors de la récupération de la liste des clients :",
+          error
+        );
+        showToast("Erreur lors de la récupération de la liste des clients", {
+          type: "error",
+        });
         setLoading(false);
       }
     };
 
     fetchClientList();
-  }, [setClientList, showToast]);
+  }, [showToast]);
+
+  // Calcule l'index du premier et du dernier client de la page actuelle
+  const indexOfLastClient = currentPage * clientsPerPage;
+  const indexOfFirstClient = indexOfLastClient - clientsPerPage;
+  const currentClients = clientList.slice(
+    indexOfFirstClient,
+    indexOfLastClient
+  );
+
+  // Change de page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="bg-cream h-screen flex flex-col items-center">
@@ -30,32 +48,69 @@ function ClientList() {
       {loading ? (
         <p>Chargement...</p>
       ) : (
-        <div className="bg-white  text-center shadow-custom pt-4 pb-8 flex flex-col font-playfair w-10/10 gap-4 ">
-        {clientList.map((client, index) => (
-            <div key={index}>
-              <p><strong>Name :</strong> {client.Name}</p>
-              <p><strong>Id :</strong> {client.Id}</p>
-              <p><strong>MainInvoicingAddress_Address1 :</strong> {client.MainInvoicingAddress_Address1}</p>
-              <p><strong>MainInvoicingAddress_Address2 :</strong> {client.MainInvoicingAddress_Address2}</p>
-              <p><strong>MainInvoicingAddress_ZipCode :</strong> {client.MainInvoicingAddress_ZipCode}</p>
-              <p><strong>MainInvoicingAddress_City :</strong> {client.MainInvoicingAddress_City}</p>
-              <p><strong>MainInvoicingAddress_State :</strong> {client.MainInvoicingAddress_State}</p>
-              <p><strong>MainInvoicingAddress_CountryIsoCode :</strong> {client.MainInvoicingAddress_CountryIsoCode}</p>
-              <p><strong>MainInvoicingContact_Civility :</strong> {client.MainInvoicingContact_Civility}</p>
-              <p><strong>MainInvoicingContact_Name :</strong> {client.MainInvoicingContact_Name}</p>
-              <p><strong>MainInvoicingContact_FirstName :</strong> {client.MainInvoicingContact_FirstName}</p>
-              <p><strong>MainInvoicingContact_Phone :</strong> {client.MainInvoicingContact_Phone}</p>
-              <p><strong>MainInvoicingContact_CellPhone :</strong> {client.MainInvoicingContact_CellPhone}</p>
-              <p><strong>MainInvoicingContact_Fax :</strong> {client.MainInvoicingContact_Fax}</p>
-              <p><strong>MainInvoicingContact_Email :</strong> {client.MainInvoicingContact_Email}</p>
-              <p><strong>MainInvoicingContact_Function :</strong> {client.MainInvoicingContact_Function}</p>
-              <p><strong>MainInvoicingAddress_WebSite :</strong> {client.MainInvoicingAddress_WebSite}</p>
-              <p><strong>FirstInvoicingDate :</strong> {client.FirstInvoicingDate}</p>
-              <p><strong>sysCreatedDate :</strong> {client.sysCreatedDate}</p>
-              <p><strong>sysCreatedUser :</strong> {client.sysCreatedUser}</p>
-              <p><strong>sysModifiedDate :</strong> {client.sysModifiedDate}</p>
-              <p><strong>sysModifiedUser :</strong> {client.sysModifiedUser}</p>
-              <p><strong>NotesClear :</strong> {client.NotesClear}</p>
+        <div className="justify-center items-center  text-center shadow-custom pt-4 pb-8 flex flex-row flex-wrap font-playfair w-10/10 gap-4 ">
+          {/* Pagination */}
+          <ul className="flex flex-row gap-2">
+            {Array.from({
+              length: Math.ceil(clientList.length / clientsPerPage),
+            }).map((_, index) => (
+              <li
+                key={index}
+                className={`page-item ${
+                  currentPage === index + 1 ? "active" : ""
+                }`}
+              >
+                <button
+                  onClick={() => paginate(index + 1)}
+                  className="page-link"
+                >
+                  {index + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+          {currentClients.map((client, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-xl border-brownperso border-4 p-2 shadow-custom mt-4 flex flex-col font-playfair w-3/10"
+            >
+              <p>
+                <strong>Nom :</strong> {client.Name} /{" "}
+                {client.MainInvoicingContact_Name}{" "}
+                {client.MainInvoicingContact_FirstName}
+                {client.Id}
+              </p>
+              <p>
+                <strong>Téléphone :</strong> {client.MainInvoicingContact_Phone}{" "}
+                {client.MainInvoicingContact_CellPhone}
+              </p>
+              <p>
+                <strong>Adresse de facturation principale (Adresse1) :</strong>{" "}
+                {client.MainInvoicingAddress_Address1}
+                {client.MainInvoicingAddress_Address2}{" "}
+                {client.MainInvoicingAddress_ZipCode}{" "}
+                {client.MainInvoicingAddress_City}{" "}
+                {client.MainInvoicingAddress_State}
+              </p>
+
+              <p>
+                <strong>Email :</strong> {client.MainInvoicingContact_Email}
+              </p>
+              <p>
+                <strong>Fonction :</strong>{" "}
+                {client.MainInvoicingContact_Function}
+              </p>
+              <p>
+                <strong>WebSite :</strong> {client.MainInvoicingAddress_WebSite}
+              </p>
+
+              <p>
+                <strong>sysCreatedUser :</strong> {client.sysCreatedUser}
+              </p>
+
+              <p>
+                <strong>Notes :</strong> {client.NotesClear}
+              </p>
             </div>
           ))}
         </div>
