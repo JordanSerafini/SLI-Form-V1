72a3bc95555c23f684d6bf7f2b9d1674ddb63b2a
JordanSerafini <jordan@solution-logique.fr>
2024-02-12 09:39:21 +0100

    :construction:

--- src/components/Page/clientList.jsx
+++ src/components/Page/clientList.jsx
@@ -57,8 +57,9 @@ function ClientList() {
       {loading ? (
         <p>Chargement...</p>
       ) : (
-        <div className="justify-center items-center text-center shadow-custom pt-4 pb-8 flex flex-row flex-wrap font-playfair w-10/10 gap-4">
-          {/* Pagination */}
+        <div className="justify-center items-center text-center shadow-custom pt-4 pb-8 flex flex-row flex-wrap font-playfair w-10/10 gap-4 ">
+          { /*---------------------------------------------------------------------------------- Map clients ---------------------------------------------------------------------------------------  */ }
+
           {currentClients.map((client, index) => (
             <div
               key={index}
@@ -70,77 +71,77 @@ function ClientList() {
                 {client.MainInvoicingContact_FirstName}
                 {client.Id}
               </p>
-              <div className="flex flex-col">
-                <strong>Téléphone :</strong>{" "}
-                <div className="flex flex-col flex-wrap gap-2">
-                  {client.MainInvoicingContact_Phone && (
-                    <>
-                      <div className="flex flex-row gap-3">
-                        <img
-                          src={logoTel}
-                          alt="logoTel"
-                          className="h-6 w-6"
-                          onClick={() =>
-                            callPhoneNumber(client.MainInvoicingContact_Phone)
-                          }
-                        />{" "}
-                        {client.MainInvoicingContact_Phone}{" "}
-                      </div>
-                    </>
-                  )}
-                  {client.MainInvoicingContact_CellPhone && (
-                    <>
-                      <div className="flex flex-row ">
-                        <img
-                          src={logoTel}
-                          alt="logoTel"
-                          className="h-6 w-6"
-                          onClick={() =>
-                            callPhoneNumber(
-                              client.MainInvoicingContact_CellPhone
-                            )
-                          }
-                        />{" "}
-                        {client.MainInvoicingContact_CellPhone}
-                      </div>
-                    </>
-                  )}
-                </div>
+              {(client.MainInvoicingContact_Phone || client.MainInvoicingContact_CellPhone) && (
+            <div className="flex flex-col">
+              <strong>Téléphone :</strong>
+              <div className="flex flex-col flex-wrap gap-2">
+                {client.MainInvoicingContact_Phone && (
+                  <div className="flex flex-row gap-2">
+                    <img
+                      src={logoTel}
+                      alt="logoTel"
+                      className="h-6 w-6"
+                      onClick={() =>
+                        callPhoneNumber(client.MainInvoicingContact_Phone)
+                      }
+                    />
+                    {client.MainInvoicingContact_Phone}
+                  </div>
+                )}
+                {client.MainInvoicingContact_CellPhone && (
+                  <div className="flex flex-row gap-2 ">
+                    <img
+                      src={logoTel}
+                      alt="logoTel"
+                      className="h-6 w-6"
+                      onClick={() =>
+                        callPhoneNumber(
+                          client.MainInvoicingContact_CellPhone
+                        )
+                      }
+                    />
+                    {client.MainInvoicingContact_CellPhone}
+                  </div>
+                )}
               </div>
-              <p>
-                <strong>Adresse de facturation principale (Adresse1) :</strong>{" "}
-                {client.MainInvoicingAddress_Address1}
-                {client.MainInvoicingAddress_Address2}{" "}
-                {client.MainInvoicingAddress_ZipCode}{" "}
-                {client.MainInvoicingAddress_City}{" "}
-                {client.MainInvoicingAddress_State}
-              </p>
-              {client.MainInvoicingContact_Email && (
-                <p>
-                  <strong>Email :</strong> {client.MainInvoicingContact_Email}
-                </p>
-              )}
             </div>
-          ))}
-        </div>
-      )}
-      <ul className="flex flex-row gap-2">
-        {Array.from({
-          length: Math.ceil(clientList.length / clientsPerPage),
-        }).map((_, index) => (
-          <li
-            key={index}
-            className={`page-item ${currentPage === index + 1 ? "active" : ""}`}
-          >
-            <button onClick={() => paginate(index + 1)} className="page-link">
-              {index + 1}
-            </button>
-          </li>
+            )}
+            <p>
+              <strong>Adresse:</strong>{" "}
+              {client.MainInvoicingAddress_Address1}
+              {client.MainInvoicingAddress_Address2}{" "}
+              {client.MainInvoicingAddress_ZipCode}{" "}
+              {client.MainInvoicingAddress_City}{" "}
+              {client.MainInvoicingAddress_State}
+            </p>
+            {client.MainInvoicingContact_Email && (
+              <p>
+                <strong>Email :</strong> {client.MainInvoicingContact_Email}
+              </p>
+            )}
+          </div>
         ))}
-      </ul>
-      <LogoutBtn />
-    </div>
-  );
+      </div>
+    )}
+
+    { /*-------------------------------------------------------------------------------------- Pagination -----------------------------------------------------------------------------------------  */ }
+    <ul className="flex flex-row gap-2">
+      {Array.from({
+        length: Math.ceil(clientList.length / clientsPerPage),
+      }).map((_, index) => (
+        <li
+          key={index}
+          className={`page-item ${currentPage === index + 1 ? "active" : ""}`}
+        >
+          <button onClick={() => paginate(index + 1)} className="page-link">
+            {index + 1}
+          </button>
+        </li>
+      ))}
+    </ul>
+    <LogoutBtn />
+  </div>
+);
 }
 
 export default ClientList;
