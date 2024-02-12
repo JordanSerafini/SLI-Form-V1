9fc35ee8ed76913e4c599fb6f727a2a9b10b1fa1
JordanSerafini <jordan@solution-logique.fr>
2024-02-12 16:49:06 +0100

    :sparkles: work on swap db local to online pgsql

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -48,7 +48,7 @@ function Home() {
   // Message de bienvenue
   useEffect(() => {
     if (userData) {
-      showToast(`Bienvenue ${userData.name} ,`, {
+      showToast(`Bonjour ${userData.name} ,`, {
         position: "bottom-center",
         autoClose: 3000,
       });
