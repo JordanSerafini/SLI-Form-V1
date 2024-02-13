8eddd951c51a6455f4f7c69ee23d8e59459a1d9d
JordanSerafini <jordan@solution-logique.fr>
2024-02-13 15:29:43 +0100

    :sparkles: working :construction:

--- src/components/Page/client/clientList.jsx
+++ src/components/Page/client/clientList.jsx
@@ -96,7 +96,7 @@ function ClientList() {
               className="bg-white rounded-xl border-brownperso border-4 p-2 shadow-custom mt-4 flex flex-col font-playfair items-center justify-start gap-2 overflow-y-auto h-72 w-9/10 md:w-4.5/10 sm:0 sm:h-52"
             >
               <p>
-                <strong>Nom :</strong> {client.name} /{" "}
+                <strong>Nom :</strong> {client.name} -{" "}
                 {client.maininvoicingcontact_name}{" "}
                 {client.maininvoicingcontact_firstname} {client.Id}
               </p>
