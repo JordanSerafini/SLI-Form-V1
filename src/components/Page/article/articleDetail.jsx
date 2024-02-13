79ea554fe64d9c7f680766ee24bd057e8cac840c
JordanSerafini <jordan@solution-logique.fr>
2024-02-13 16:34:07 +0100

    :construction: working on articleDetail page

--- src/components/Page/article/articleDetail.jsx
+++ src/components/Page/article/articleDetail.jsx
@@ -1,7 +1,6 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import RatingContext from "../../../context/RatingContext";
 import HomeBtn from "../../Button/HomeBtn";
-//import BackBtn from "../../Button/BackBtn";
 import { useLocation } from "react-router-dom";
 
 function ArticleDetail() {
@@ -9,6 +8,9 @@ function ArticleDetail() {
   const { articleId } = useContext(RatingContext);
   const location = useLocation();
   const { updateLastPath } = useContext(RatingContext);
+
+  // État pour gérer l'affichage du prix d'achat
+  const [showPurchasePrice, setShowPurchasePrice] = useState(false);
 
   useEffect(() => {
     updateLastPath(location.pathname);
@@ -22,23 +24,23 @@ function ArticleDetail() {
   }
 
   return (
-    <div className="bg-cream h-[100vh]  overflow-x-hidden flex flex-col items-center p-4">
+    <div className="bg-cream h-[100vh] overflow-x-hidden flex flex-col items-center p-4">
       <div className="item-center bg-white h-9.5/10 w-9.5/10 rounded-xl border-brownperso border-4 p-2 shadow-custom mt-4 flex flex-col font-playfair items-center justify-start gap-2">
         <h2 className="font-bold border-b-2 border-blue-strong pb-2 pt-2">
           {article.caption} 
         </h2>
-        <div className="pt-6 flex flex-col  items-center gap-2">
-          <p>
-            Prix HT:{" "}
-            <span className="font-bold">{article.salepricevatexcluded}</span>
-          </p>
-          <p>
-            Prix TTC:{" "}
-            <span className="font-bold">{article.salepricevatincluded}</span>
-          </p>
+        <div className="pt-6 flex flex-col items-center gap-2">
+          {/* Autres informations sur l'article */}
+          <p>Prix HT: <span className="font-bold">{article.salepricevatexcluded}</span></p>
+          <p>Prix TTC: <span className="font-bold">{article.salepricevatincluded}</span></p>
           <p>Stock: {article.realstock}</p>
-          <p><span className="font-bold">Description:</span>{article.descomclear}</p>
-          <p>Prix achat: {article.purchaseprice}</p>
+          <p><span className="font-bold">Description:</span> {article.descomclear}</p>
+          
+          {/* Élément cliquable pour afficher le prix d'achat */}
+          <div onClick={() => setShowPurchasePrice(!showPurchasePrice)} className="cursor-pointer">
+            <span className="font-bold">Prix achat:</span>
+            {showPurchasePrice ? ` ${article.purchaseprice}` : ' Cliquez pour afficher'}
+          </div>
         </div>
       </div>
       <HomeBtn />
